ade434b5ce2fad1ba27024b54c5dd64b0f7ac12a
Andri Arrieta <andriarrieta7@gmail.com>
2026-01-28 20:40:24 +0000

    Creacion de los models people y planets

--- src/models.py
+++ src/models.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
@@ -16,4 +17,37 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+
+class People(db.Model):
+    __tablename__ = "people"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    gender: Mapped[str] = mapped_column(String(120), nullable=False)
+    birdth_year: Mapped[int] = mapped_column(String(20), nullable=False)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name":self.id,
+            "gender":self.gender,
+            "bird_year": self.birdth_year
+        }
+    
+
+class Planet(db.Model):
+    __tablename__ = "planet"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(db.String(120), nullable=False)
+    climate: Mapped[str] = mapped_column(db.String(120))
+    population: Mapped[str] = mapped_column(db.String(50))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "population": self.population
         }
